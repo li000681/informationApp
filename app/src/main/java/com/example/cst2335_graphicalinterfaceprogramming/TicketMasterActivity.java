4c485ace19d257cfe38478ffe8e55b6a6c9d2639
weili <li000680@algonquinlive.com>
2020-11-14 18:04:02 -0500

    Initial commit

--- app/src/main/java/com/example/cst2335_graphicalinterfaceprogramming/TicketMasterActivity.java
+++ app/src/main/java/com/example/cst2335_graphicalinterfaceprogramming/TicketMasterActivity.java
@@ -10,5 +10,6 @@ public class TicketMasterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_master);
+
     }
 }
