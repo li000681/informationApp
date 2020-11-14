c7c70eccec69651f55e3c92c51d5d1a9336dfa43
weili <li000680@algonquinlive.com>
2020-11-14 18:04:02 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cst2335_graphicalinterfaceprogramming/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.cst2335_graphicalinterfaceprogramming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button TicketMasterButton = findViewById(R.id.button1);
+        Intent nextPage = new Intent(MainActivity.this, TicketMasterActivity.class);
+        TicketMasterButton.setOnClickListener(click->
+        {
+            startActivity(nextPage);
+        });
+    }
+}
