5d0457e21c2ebe8d01f739ba33dc90099c374c24
junel <li000682@algonquinlive.com>
2020-11-18 12:05:01 -0500

    test

---
+++ app/src/main/java/com/example/cst2335_graphicalinterfaceprogramming/Covid19Acitivity.java
@@ -0,0 +1,14 @@
+package com.example.cst2335_graphicalinterfaceprogramming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Covid19Acitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covid19_acitivity);
+    }
+}
