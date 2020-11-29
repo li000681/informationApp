30aedcf6685c215bc20aed3a0520d8ddc9497890
junel <li000682@algonquinlive.com>
2020-11-29 10:48:49 -0500

    changed home page

--- app/src/main/java/com/example/cst2335_graphicalinterfaceprogramming/MainActivity.java
+++ app/src/main/java/com/example/cst2335_graphicalinterfaceprogramming/MainActivity.java
@@ -1,42 +1,100 @@
 package com.example.cst2335_graphicalinterfaceprogramming;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar tBar = (Toolbar)findViewById(R.id.hometoolbar);
+        //This loads the toolbar, which calls onCreateOptionsMenu below:
+        setSupportActionBar(tBar);
+        DrawerLayout drawer = findViewById(R.id.homedrawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, tBar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-        Button TicketMasterButton = findViewById(R.id.button1);
-        Intent nextPage = new Intent(MainActivity.this, TicketMasterActivity.class);
-        TicketMasterButton.setOnClickListener(click->
+        NavigationView navigationView = findViewById(R.id.homenav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.homepagemenu, menu);
+        return true;
+    }
+
+    /**
+     * This hook is called whenever an item in your options menu is selected.
+     * The default implementation simply returns false to have the normal
+     * processing happen (calling the item's Runnable or sending a message to
+     * its Handler as appropriate).
+     * <p>Derived classes should call through to the base class for it to
+     * perform the default menu handling.</p>
+     * @param item The menu item that was selected.
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId())
         {
-            startActivity(nextPage);
-        });
-
-        Button TheAudioDbButton = findViewById(R.id.button4);
-        Intent audioDb = new Intent(this, TheAudioDatabase.class);
-        TheAudioDbButton.setOnClickListener(click->
+            //what to do when the menu item is selected:
+            case R.id.item1:
+                startActivity(new Intent(this, TicketMasterActivity.class));
+                break;
+            case R.id.item2:
+                startActivity(new Intent(this,RecipeSearchToolBar.class));
+                break;
+            case R.id.item3:
+                startActivity(new Intent(this,Covid19Activity.class));
+                break;
+            case R.id.item4:
+                startActivity(new Intent(this, TheAudioDatabase.class));
+                break;
+        }
+        return true;
+    }
+    @Override
+    public boolean onNavigationItemSelected( MenuItem item) {
+        switch(item.getItemId())
         {
-            startActivity(audioDb);
-        });
+            case R.id.item1:
+                startActivity(new Intent(this, TicketMasterActivity.class));
+                break;
+            case R.id.item2:
+                startActivity(new Intent(this,RecipeSearchToolBar.class));
+                break;
+            case R.id.item3:
+                startActivity(new Intent(this,Covid19Activity.class));
+                break;
+            case R.id.item4:
+                startActivity(new Intent(this,TheAudioDatabase.class));
+                break;
 
-        Button covid=findViewById(R.id.button3);
-        covid.setOnClickListener(clk->
-                startActivity(new Intent(MainActivity.this, Covid19Activity.class)));
+        }
 
-        /** Button ReceipeSearchButton will lead to the recipe search page.*/
-        Button ReceipeSearchButton = findViewById(R.id.button2);
-        Intent nextPage1 = new Intent(MainActivity.this, RecipeSearchToolBar.class);
-        ReceipeSearchButton.setOnClickListener(click->
-        {
-            startActivity(nextPage1);
-        });
+        DrawerLayout drawerLayout = findViewById(R.id.homedrawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
